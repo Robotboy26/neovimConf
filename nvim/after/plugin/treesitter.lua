be9fca6c23bebe26911927ac5b863deb72ee66ce
robotboy26 <robotboygamer26@gmail.com>
2024-01-17 21:51:35 -0800

    Changes to be committed:

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "java", "typescript", "haskell" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "java", "typescript", "haskell", "zig" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- nvim/lua/robot/colors.lua
+++ nvim/lua/robot/colors.lua
@@ -1,4 +1,5 @@
 function ColorMyPencils(color)
+    -- rose-pine is the backup color scheme
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
