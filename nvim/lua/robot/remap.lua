1d4087f034c2dccf76427c7337d31cb752b916b5
robotboy26 <robotboygamer26@gmail.com>
2024-01-24 06:10:31 -0800

    Changes to be committed:

--- nvim/lua/robot/remap.lua
+++ nvim/lua/robot/remap.lua
@@ -7,9 +7,9 @@ vim.keymap.set("n", "<space>", "<Nop>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "<C-d>zz")
+vim.keymap.set("n", "K", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
