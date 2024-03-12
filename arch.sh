2283443861f9bbeca68c203c11f6d6885abc4bd1
Cole Pearson <113060096+Robotboy26@users.noreply.github.com>
2024-03-12 21:34:49 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,1 +1,7 @@
+rm -rf ~/.config/nvim
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ 
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 cp -r nvim ~/.config/
