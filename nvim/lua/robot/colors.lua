b0248f910065d796d1b87b843f770c8e57a5c534
Cole Pearson <113060096+Robotboy26@users.noreply.github.com>
2024-03-13 02:59:53 +0000

    Update colors.lua

--- nvim/lua/robot/colors.lua
+++ nvim/lua/robot/colors.lua
@@ -1,8 +1,5 @@
 function ColorMyPencils(color)
     -- rose-pine is the backup color scheme
 	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.cmd.colorscheme(moonlight)
 end
